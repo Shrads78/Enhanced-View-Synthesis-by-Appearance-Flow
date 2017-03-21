68ba4e761f021da1d943cc78568e7021309eecc4
shrads93 <agrawalshradha78@gmail.com>
2017-03-20 20:35:05 -0700

    Removing vairable for sequential saving of weights.

--- code/viewsyn_experiments.py
+++ code/viewsyn_experiments.py
@@ -22,7 +22,7 @@ def run_autoencoder(test_images):
 	try:
 		hist = train_model.train_network(autoencoder, f_generate_list, f_generate_data)
 	finally:
-		autoencoder.save('../model/weights-interrupt-autoencoder.' + str(len(os.listdir('../model/'))) + '.hdf5')
+		autoencoder.save('../model/weights-interrupt-autoencoder.hdf5')
 
 	#test autoencoder
 	
@@ -39,7 +39,7 @@ def run_transformed_autoencoder(test_images):
 	try:
 		hist = train_model.train_network(t_autoencoder, f_generate_list, f_generate_data)
 	finally:
-		t_autoencoder.save('../model/weights-interrupt-t_autoencoder.' + str(len(os.listdir('../model/'))) + '.hdf5')
+		t_autoencoder.save('../model/weights-interrupt-t_autoencoder.hdf5')
 
 
 	#test transformed autoencoder
@@ -57,7 +57,7 @@ def run_replication(test_images):
 	try:
 		hist = train_model.train_network(replication_net, f_generate_list, f_generate_data)
 	finally:
-		replication_net.save('../model/weights-interrupt-replication_net.' + str(len(os.listdir('../model/'))) + '.hdf5')
+		replication_net.save('../model/weights-interrupt-replication_net.hdf5')
 
 
 	#test replication network
@@ -77,7 +77,7 @@ def run_five_channel_network(test_images):
 	try:
 		hist = train_model.train_network(five_channel_net, f_generate_list, f_generate_data)
 	finally:
-		five_channel_net.save('../model/weights-interrupt-five_channel_net.' + str(len(os.listdir('../model/'))) + '.hdf5')
+		five_channel_net.save('../model/weights-interrupt-five_channel_net.hdf5')
 
 
 
